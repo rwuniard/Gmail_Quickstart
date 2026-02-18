357a881af59409989619792906b1adc66e82f740
rwuniard <rwuniard@hotmail.com>
2026-02-18 17:34:02 -0500

    Implement main function to send unread Gmail messages to ActiveMQ queue, enhancing logging and connection management. Add environment variable support for Producer configuration.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from logger import setup_logging  
 import logging
 
+import os
+
 from messaging import Producer
 
 load_dotenv()
@@ -12,18 +14,43 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 
+
+
+
+
 def main():
-   
+    """
+    Main function to send the unread messages from the Gmail API to the ActiveMQ queue
+    """
     READ_ONLY_SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
     gmail_client = GmailClient(READ_ONLY_SCOPES)
     gmail_client.authenticate()
+
+    producer = Producer(
+        host=os.getenv('HOST'),
+        port=int(os.getenv('PORT')),
+        username=os.getenv('USERNAME'),
+        password=os.getenv('PASSWORD'),
+        destination=os.getenv('DESTINATION')
+    )
+
+    # Get the unread messages from the Gmail API
     unread_messages = gmail_client.get_unread_messages_from_LinkedIn_JobAlerts(max_results=20)
+
+
     for unread_message in unread_messages:
         logger.info("########################################################")
-        print(unread_message.model_dump_json())
-        with open(f"unread_messages_debug_{unread_message.id}.json", "w") as f:
-            f.write(unread_message.model_dump_json())
-        logger.info("--------------------------------")
+        logger.info(unread_message.model_dump_json())
+        # Send the unread messages to the ActiveMQ queue
+        producer.send_message(unread_message.model_dump_json())
+        # with open(f"unread_messages_debug_{unread_message.id}.json", "w") as f:
+        #     f.write(unread_message.model_dump_json())
+        # print("--------------------------------")
+    logger.info("Unread messages sent to the ActiveMQ queue successfully")
+    producer.close_connection()
+    logger.info("Connection closed successfully")
 
-# Send the unread messages to the ActiveMQ queue
-main()
+# call the main function
+if __name__ == "__main__":
+    main()
+    logger.info("Main function completed successfully")
